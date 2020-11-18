9be880aa1ffe722e42ea8fbf082aca18040ce915
Capt JonLee <jon.lee.choon.yang@gmail.com>
2020-11-18 22:03:17 +0800

    Added XPmul, XPDiv, and debugged the XPsub as it was not changing the length

--- XP.h
+++ XP.h
@@ -8,16 +8,22 @@
 #define N  100         // maximum number of digits supported
 #define NN (2*N + 1)   // number of digits allocated
 
-typedef struct {
+typedef struct {        // struct to help with the extended precision arithmetic
    unsigned char digit[NN];
    int length;
 } XP;
 
-XP XPinitDecimal(std::string s);
-XP XPadd(XP a, XP b);
-XP XPrand(int n);
-bool XPisOdd(XP a);
-int XPgreater(XP a, XP b);
-int XPless(XP a, XP b);
-int XPeq(XP a, XP b);
-XP XPsub (XP a, XP b);
+typedef struct {        // struct to help with the calculation of Division
+   XP quotient;
+   XP remainder;
+} Div;
+
+XP XPinitDecimal(std::string s);    // Initialises a extended precision decimal based on a input string, up to a 100 digits
+XP XPadd(XP a, XP b);               // Prints out the extended precision number in full
+XP XPrand(int n);                   // Generate a pseudo-random number based on the clock, of length n
+bool XPisOdd(XP a);                 // Determines if a extended precision number is odd   
+int XPgreater(XP a, XP b);          // If a is greater than b, then return 1. Else it returns -1
+int XPless(XP a, XP b);             // If a is less than b, then return 1. Else it returns -1
+int XPeq(XP a, XP b);               // If a is equal to b, then return 1. Else it returns -1
+XP XPsub(XP a, XP b);               // Multiplication of a and b
+Div XPdiv(XP a, XP b);              // Division of a by b, returns the quotient and remainder in Div struct
--- xp.cpp
+++ xp.cpp
@@ -1,40 +1,49 @@
+/*
+This program is a helper program that will help to calculate integer arithmetic for up to a 100 digits in length
+This is necessary for the calculations in the RSA algorithm
+*/
+
 #include "XP.h"
 
 using namespace std;
 
-
-XP XPinitDecimal(string s) {
+XP XPinitDecimal(string s) {     // Initialises a extended precision decimal based on a input string, up to a 100 digits
    XP a;
    int length = s.size(); 
    a.length = length;
    if (length > N) throw "length of input is larger than N!";
 
-    for (int i = 0; i < length; i++) {
-       if (s[i] > '9' || s[i] < '0') throw "The digits are out of range";
-        a.digit[NN - length + i] = s[i] - '0';
-    }
-    for (int i = 0; i < NN - length; i++) {
+   for (int i = 0; i < length; i++) {
+       if (s[i] > '9' || s[i] < '0') throw "The digits are out of range"; // catch any digit that is not between '0' and '9'
+        a.digit[NN - length + i] = s[i] - '0';      // make all the char between 0 and 9 in ASCII
+   }
+   for (int i = 0; i < NN - length; i++) {
        a.digit[i] = 0;
-    }
-    return a;
+   }
+   return a;
 }
 
-void XPshowDecimal(XP c) {
+void XPshowDecimal(XP c) {       // Prints out the extended precision number in full
    for (int i = 0; i < c.length; i++) {
       printf("%d", c.digit[NN - c.length + i]);
       }
       cout << endl;
 }
 
-XP XPadd(XP a, XP b) {
-   unsigned char sum[NN] = {0};
-   XP c = XPinitDecimal("0");
+XP XPadd(XP a, XP b) {        // Sums together 2 extended precision numbers
+   unsigned char sum[NN] = {0};     // Create a new char array to store the overflow when 2 digits sum to more than 10
+   XP c = XPinitDecimal("0");       
+   int longer = max(a.length, b.length);
 
    for (int i = NN - 1; i > 0; i--) {
-      char x = a.digit[i] + b.digit[i] + sum[i];
-      if (x > 0 && i < NN - 1) c.length++;
+      if (i < NN - longer - 1) break;
+      char x = a.digit[i] + b.digit[i] + sum[i];   
+      if (i < NN - 1) {
+         if (i == NN - longer - 1 && x == 0) break;
+         c.length++;       // Increase the length of c for digits more than length 1 and up to 1 digit pass the longer length
+      }
       if (x > 9) {
-         sum[i - 1] = 1;
+         sum[i - 1] = 1;   // Overflow when 2 digits sum to more than 10
          x -= 10;
       }
       c.digit[i] = x;
@@ -42,7 +51,7 @@ XP XPadd(XP a, XP b) {
    return c;
 }
 
-XP XPrand(int n) {
+XP XPrand(int n) {      // Generate a pseudo-random number based on the clock, of length n
    int x;
    stringstream ss;
 
@@ -55,12 +64,12 @@ XP XPrand(int n) {
    return out;
 }
 
-bool XPisOdd(XP a) {
+bool XPisOdd(XP a) {    // Determines if a extended precision number is odd
    char lastDigit = a.digit[NN - 1];
    return lastDigit % 2 == 1;
 }
 
-int XPgreater(XP a, XP b) {
+int XPgreater(XP a, XP b) {      // If a is greater than b, then return 1. Else it returns -1
    if (a.length > b.length) {return 1;}
    if (a.length == b.length) {
       for (int i = 0; i < a.length; i++) {
@@ -72,7 +81,7 @@ int XPgreater(XP a, XP b) {
    return -1;
 }
 
-int XPless(XP a, XP b) {
+int XPless(XP a, XP b) {         // If a is less than b, then return 1. Else it returns -1
    if (a.length < b.length) {return 1;}
    if (a.length == b.length) {
       for (int i = 0; i < a.length; i++) {
@@ -84,7 +93,7 @@ int XPless(XP a, XP b) {
    return -1;
 }
 
-int XPeq(XP a, XP b) {
+int XPeq(XP a, XP b) {        // If a is equal to b, then return 1. Else it returns -1
    if (a.length == b.length) {
       for (int i = 0; i < a.length; i++) {
          if (i == a.length - 1 && a.digit[NN - a.length + i] == b.digit[NN - a.length + i]) {return 1;}
@@ -95,45 +104,106 @@ int XPeq(XP a, XP b) {
    return -1;
 }
 
-XP XPsub(XP a, XP b) {
+XP XPsub(XP a, XP b) {        // Subtract b from a
    if (XPless(a, b) == 1) throw "first XP is less than 2nd XP!";
-   unsigned char sub[NN] = {0};
+   unsigned char sub[NN] = {0};     // Char array to store if next digit in a is required to +10 to current digit
 
    for (int i = NN - 1; i >= NN - a.length; i--) {
-      if (a.digit[i] == 0) {
-         a.digit[i] += 10;
+      if (a.digit[i] == 0) {        // Check if the least significant digit of a is 0, if so +10
+         a.digit[i] += 10;      
          sub[i - 1]++;
       }
-      a.digit[i] -= sub[i];
-      if (i == NN - a.length && a.digit[i] == 0) {
-         a.length--;
-      }
-      if (a.digit[i] < b.digit[i]) {
+      a.digit[i] -= sub[i];         // Minus if this digit owes 1
+      if (a.digit[i] < b.digit[i]) {  // Check if a is less than b, if so +10
          a.digit[i] += 10;
          sub[i - 1]++;
       }
       a.digit[i] -= b.digit[i];
    }
+   int i = NN - a.length;
+   while (a.digit[i] == 0 && i < NN) { // Make sure that the length of a is correct!
+      a.length--;
+      i++;
+   }
    return a;
+}
+
+XP XPmul(XP a, XP b) {        // Multiplication of a and b
+   int count = 0;       // count is used to store which digit of b we are on
+   XP sum = XPinitDecimal("0");
+
+   for (int x = NN - 1; x >= NN - b.length; x--) {       // For each digit in b
+      unsigned char carryOver[NN] = {0};     // carryOver char array to the next digit of a
+      unsigned char mul[NN] = {0};           // Used to store the multiplication answer
+      int length = 0;                        // Store length of multiplication of a for this digit of b
+      
+      for (int y = 0; y <= a.length; y++) {        // For each digit in a
+         unsigned char temp = b.digit[x] * a.digit[NN - 1 - y] + carryOver[NN - 1 - y];
+         if (temp > 9) {         // carryOver if temp is more than 9
+            carryOver[NN - 2 - y] = temp / 10;
+            temp %= 10;
+         }
+         mul[NN - 1 - y] = temp;    
+         if (y < a.length) length++;            // Up to the length of a
+         if (y == a.length && temp != 0) length++; // Check if length of multiplication is longer than a
+      }
+      string add;
+      for (int i = NN - length; i < NN; i++){      
+         add.append(sizeof(char), mul[i] + '0');     // Generate the multiplication string, not forgetting to add back '0'
+      }
+      add.append(count, '0');             // Multiply by the correct number of 10s in b
+      count++;
+      sum = XPadd(sum, XPinitDecimal(add));  // add the multiplication into the total sum
+   }
+   return sum;
+}
+
+Div XPdiv(XP a, XP b) {          // Division of a by b, returns the quotient and remainder in Div struct
+   Div result;
+
+   if (XPless(a, b) == 1) {      // if (a < b)
+      result.quotient = XPinitDecimal("0");
+      result.remainder = a;
+      return result;             // return (0, a)
+   }
+   
+   result = XPdiv(a, XPmul(b, XPinitDecimal("2")));                     // (q, r) = div(a, 2b)
+   result.quotient = XPmul(result.quotient, XPinitDecimal("2"));        //  q = 2q
+   
+   if (XPless(result.remainder, b) == 1) {      //  if (r < b) return (q, r)
+      return result;
+   }
+   
+   else {         // else return (q + 1, r - b)
+      result.quotient = XPadd(result.quotient, XPinitDecimal("1"));
+      result.remainder = XPsub(result.remainder, b);
+   }
 }
 
 int main() {
    XP a, b, c;
    
+   
    try {
-      a = XPinitDecimal("1234567890");   // a   = 123456789
-      b = XPinitDecimal("545454545");    // b   =  54545454
-      c = XPadd(a, b);                  // c   =  a + b
-      XPshowDecimal(c);                 // print 178002243
-      XPshowDecimal(XPrand(99));
-      printf(XPisOdd(c) ? "true" : "false");
-      cout << endl;
-      printf("%d\n", XPgreater(a, b));
-      printf("%d\n", XPless(b, a));
-      printf("%d\n", XPeq(a, a));
-      XPshowDecimal(XPsub(a, b));
+      string in = "";
+      int n = 0;
+      while (true) // Code for debugging
+      {
+         Div result;
+         cout << "a: ";
+         getline(cin, in);
+         a = XPinitDecimal(in);
+         cout << "b: ";
+         getline(cin, in);
+         b = XPinitDecimal(in);
+         result = XPdiv(a, b);
+         cout << "quotient: ";
+         XPshowDecimal(result.quotient);
+         cout << "remainder: ";
+         XPshowDecimal(result.remainder);
+      }
+      
    } catch (string msg) {
       cout << msg << endl;
    }
-
 }
